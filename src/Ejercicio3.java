457038f95cbc4ae951434dd225b52c1f25f8ad69
LeonorYumi <Leonoryumi@gmail.com>
2025-10-05 21:05:46 -0500

    Ejercicios

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
 public class Ejercicio3 {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("===Tabla de Multiplicar===");
+        System.out.printf("Ingrese un numero (1 al 10): ");
+        int num = scanner.nextInt();
+        if (num >= 1 && num <= 10) {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(num + " * " + i + " = " + (num * i));
+            }
+        }else{
+                System.out.println("El numero ingresado no esta en el rango (1 al 10).");
+            }
+        }
+    }
+
--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
 public class Ejercicio4 {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Solicitar nombre
+        System.out.print("Ingrese su nombre: ");
+        String nombre = scanner.next();
+
+        // Solicitar edad
+        System.out.print("Ingrese su edad: ");
+        int edad = scanner.nextInt();
+
+        // Solicitar altura
+        System.out.print("Ingrese su altura en metros: ");
+        double altura = scanner.nextDouble();
+
+        // Verificar si cumple los requisitos
+        if (edad > 18 && altura > 1.60) {
+            System.out.println("Aceptado para el equipo. ");
+            } else {
+                System.out.println("No cumple los requisitos. ");
+            }
+        }
+
+    }
